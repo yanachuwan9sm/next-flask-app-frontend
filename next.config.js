c61bf58b1a98579580aea71f0fa84e4fddbf96b1
yanachuwan9sm <gurem919@icloud.com>
2022-02-26 12:31:29 +0900

    setting: Docker環境でのホットリロードに対応する

--- next.config.js
+++ next.config.js
@@ -1,6 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  // webpackDevMiddlewareの設定を追加
+  // HOKIDAR_USEPOLLING=trueにするだけではDocker環境でホットリロードされなかった
+  webpackDevMiddleware: (config) => {
+    config.watchOptions = {
+      poll: 800,
+      aggregateTimeout: 300,
+    };
+    return config;
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
