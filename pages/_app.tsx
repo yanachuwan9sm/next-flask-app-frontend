c4a7e3cb6b77969b2a0482e9620d590e48c9e634
yanachuwan9sm <gurem919@icloud.com>
2022-03-01 23:59:44 +0900

    SSGおよびSSRでの速度テスト

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,32 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import "../styles/globals.css";
+import React from "react";
+import type { AppProps } from "next/app";
+import { ChakraProvider, extendTheme } from "@chakra-ui/react";
+import App from "next/app";
+import Head from "next/head";
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
-}
+const config = {
+  useSystemColorMode: false,
+  initialColorMode: "dark",
+};
 
-export default MyApp
+const darkTheme = extendTheme({ config });
+
+const MyApp: React.FC<AppProps> = ({ Component, pageProps }) => {
+  return (
+    <>
+      {/* <Head>
+        <script
+          async
+          src={`https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=${process.env.NEXT_PUBLIC_GOGGLE_ADSENSE}`}
+          crossOrigin="anonymous"
+        ></script>
+      </Head> */}
+      <ChakraProvider theme={darkTheme}>
+        <Component {...pageProps} />
+      </ChakraProvider>
+    </>
+  );
+};
+
+export default MyApp;
